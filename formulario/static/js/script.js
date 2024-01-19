df049eb14beb7a3de3b256f1876c67d0814fb023
Job Rodriguez <101521413+JobRodriguez@users.noreply.github.com>
2024-01-18 20:21:27 -0600

    Add files via upload

---
+++ formulario/static/js/script.js
@@ -0,0 +1,14 @@
+
+function actualizar(){
+    window.location.href = 'index.html';
+
+}
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
